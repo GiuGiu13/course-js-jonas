c6c6143590903580ac04cac1eaab79b870578d71
Giusy <luminal@ymail.com>
2020-01-03 15:49:39 +0000

    initialization function

--- budget-app-project/app.js
+++ budget-app-project/app.js
@@ -33,30 +33,42 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+    
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+    
+        });
+    };
+
+
+
+    
 
     var ctrlAddItem = function () {
         //1. get the filed input data
         var input = UICtrl.getInput();
-        console.log(input)
 
         //2. add the item to the budget controller
         //3. add the item to the UI
         //4. calculate the budget
         //5. display the budget on the UI
 
+    };
+
+    return {
+        init: function(){
+            console.log('App started');
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-
-    })
-
-
-
 })(budgetController, UIController);
+
+controller.init();
