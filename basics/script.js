dc07276f36cc697f5b532061a82c4164b9c808e3
Giusy <luminal@ymail.com>
2020-01-03 10:50:01 +0000

    variable mutation

--- basics/script.js
+++ basics/script.js
@@ -1,9 +1,36 @@
+/***********************************
+ * Variables and data type
+ */
 // console.log('ciao');
-var firstName = 'Giusy';
-console.log(firstName);
+// var firstName = 'Giusy';
+// console.log(firstName);
 
-var lastName = 'Smith';
+// var lastName = 'Smith';
+// var age = 28;
+// var fullAge = true;
+
+// console.log(fullAge);
+
+/***********************************
+ * Variables mutation and type coercion
+ */
+
+var firstName = "John";
 var age = 28;
-var fullAge = true;
 
-console.log(fullAge);
+//type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job ='teacher';
+isMarried =  false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); 
+
+//variable mutation
+age = 'tweenty eight';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); 
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
