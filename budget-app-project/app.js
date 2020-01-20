59089d01eb149a373cb574db1d178c08b549f21a
Giusy <luminal@ymail.com>
2020-01-20 11:50:38 +0000

    update percentage in ui controller

--- budget-app-project/app.js
+++ budget-app-project/app.js
@@ -178,7 +178,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
 
     };
@@ -245,6 +246,29 @@ var UIController = (function () {
             }
         },
 
+        displayPercentages: function (percentages) {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            //reasable function
+            var nodeListForEach = function (list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+
+            };
+
+        nodeListForEach(fields, function (current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---'
+                }
+
+            });
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -292,7 +316,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentage();
 
         //3. update the UI with the new percentages
-        console.log(percentages)
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function () {
