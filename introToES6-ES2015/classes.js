242eebedca0224d229e23ea29d08dc0458dd9e30
Giusy <luminal@ymail.com>
2020-01-27 12:56:15 +0000

    classes

---
+++ introToES6-ES2015/classes.js
@@ -0,0 +1,42 @@
+////classes ---> synthetic sugar
+
+//ES5
+var Person5 = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function(){
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+
+//ES6
+class Person6 {
+    constructor(name, yearOfBirth, job){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge(){
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+    static greeting() {
+        console.log('Hey there!')
+    }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+
+//static method are attacched to the class not inerithed but class instances, created thorught that class
+//we cannot call the greetings with john because it's not inherit
+Person6.greeting();
+
+
+
