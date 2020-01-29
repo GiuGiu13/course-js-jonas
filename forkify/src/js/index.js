04de51b29c141c3807601c3de977469587fe7a4e
Giusy <luminal@ymail.com>
2020-01-29 11:28:52 +0000

    axios added / first API call

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -11,3 +11,21 @@
 // console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}, ${str}`)
 
 
+//if you have problem with CORS use
+//https://alfilatov.com/posts/run-chrome-without-cors/
+//open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
+
+import axios from 'axios';
+//https://forkify-api.herokuapp.com/api/search
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    try {
+        const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = res.data.recipes
+        console.log(recipes)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+getResults('pasta');
