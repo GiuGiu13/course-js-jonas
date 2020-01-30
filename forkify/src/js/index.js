c45ff8d939c5848582c29241699a6ae5ef7afbcc
Giusy <luminal@ymail.com>
2020-01-30 12:17:15 +0000

    building search view - part 1

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -11,6 +11,11 @@
 // console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}, ${str}`)
 
 import Search from './models/Search';
+//here we want everything from search view module
+//so we are going to have every variable from search view stored in the variable searchView
+import * as searchView from './views/searchView';
+
+import { elements } from './views/base'
 
 //all the data in this moment is a state, and we want that to be in one place, like one central variable object
 //that contain all the data in one moment, in one place, in  one object
@@ -25,26 +30,28 @@ const state = {};
 
 const controlSearch = async () =>{
     //1) get query from view
-    const query = "pizza" //TODO
-    
+    const query = searchView.getInput() //TODO
+
     if(query){
         //2) new search object and add to state
         state.search = new Search(query);
 
         //3) prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         //4) search for recipes
         await state.search.getResults();
 
         //5) render results on UI
-        console.log(state.search.result)
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
 
 
-search.getResults();
+// search.getResults();
